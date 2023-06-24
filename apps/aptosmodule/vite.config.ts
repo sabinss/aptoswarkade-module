7da6c00f95fe99897b6d8539880643e2c4858e53
Lovish Hamal <lovish@noveltytechnology.com>
2023-06-24 23:27:53 +0545

    added turbo repo

--- apps/aptosmodule/vite.config.ts
+++ apps/aptosmodule/vite.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
   css: {
     preprocessorOptions: {
       less: {
         javascriptEnabled: true,
-        additionalData: '@root-entry-name: variable;',
+        additionalData: "@root-entry-name: variable;",
       },
     },
   },
