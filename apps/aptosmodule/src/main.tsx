6edc1dde8ae5fcdf0f5ffccd88a785ed419a3065
Lovish Hamal <lovish@noveltytechnology.com>
2023-06-26 20:56:36 +0545

    fixed symlink

--- apps/aptosmodule/src/main.tsx
+++ apps/aptosmodule/src/main.tsx
@@ -1,10 +1,10 @@
-import ReactDOM from 'react-dom/client';
-import App from './App.js';
-import React from 'react';
-import Main from "aptoswarkade/src";
+// import ReactDOM from 'react-dom/client';
+// import App from './App.js';
+// import React from 'react';
+// import Main from "aptoswarkade/src";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  // <React.StrictMode>
-  <Main />
-  // </React.StrictMode>
-);
+// ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+//   // <React.StrictMode>
+//   <Main />
+//   // </React.StrictMode>
+// );
