b95c44f9cd98823952402cbb91ee914633bb47d3
sabin <sabin@noveltytechnology.com>
2023-06-27 09:57:30 +0545

    code comment

--- apps/aptosmodule/src/App.tsx
+++ apps/aptosmodule/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Module from "aptoswarkade/src";
+import Module from 'aptoswarkade/src';
 
 function App() {
   return (
